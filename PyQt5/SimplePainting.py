6412a2f8516c57ddf78490e2ed1500f33bf50fa4
ddkhoa87 <ddkhoa87@gmail.com>
2017-07-15 12:24:08 +0800

    Create SimplePainting.py

---
+++ PyQt5/SimplePainting.py
@@ -0,0 +1,51 @@
+SimplePainting.py
+d
+
+You uploaded an item
+Jun 19
+Text
+SimplePainting.py
+No recorded activity before June 19, 2017
+
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 16 14:40:29 2017
+
+@author: DangKhoa
+
+PyQt image: QPixmap
+"""
+import sys
+from PyQt5.QtWidgets import (QWidget, QHBoxLayout, 
+    QLabel, QApplication)
+from PyQt5.QtGui import QPixmap
+
+class Example(QWidget):
+    
+    def __init__(self):
+        super().__init__()
+        
+        self.initUI()
+        
+        
+    def initUI(self):      
+
+        hbox = QHBoxLayout(self)
+        pixmap = QPixmap("aircraft_icon.png")
+
+        lbl = QLabel(self)
+        lbl.setPixmap(pixmap)
+
+        hbox.addWidget(lbl)
+        self.setLayout(hbox)
+        
+        self.move(300, 200)
+        self.setWindowTitle('Sample image')
+        self.show()        
+        
+        
+if __name__ == '__main__':
+    
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
