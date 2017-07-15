6e2dc673961bc0124e8f9d71abf6af75c4e90ec6
ddkhoa87 <ddkhoa87@gmail.com>
2017-07-15 12:50:25 +0800

    Create OpenCVForEach.cpp

---
+++ OpenCVForEach.cpp
@@ -0,0 +1,38 @@
+#include <opencv2/opencv.hpp>
+
+void complicatedThreshold(Pixel &pixel)
+{
+  // Do something fancy.
+}
+
+struct Operator
+{
+  void operator ()(Pixel &pixel, const int * position) const
+  {
+    // Perform a simple threshold operation
+   	complicatedThreshold(pixel);
+  }
+};
+
+// Single thread
+for (int n = 0; n < numTrials; n++)
+{
+  image2.forEach<Pixel>(Operator());
+}
+
+// C++11 Multi-thread
+#if __cplusplus >= 201103L || (__cplusplus < 200000 && __cplusplus > 199711L)
+  
+  for (int n = 0; n < numTrials; n++)
+  {
+    // Parallel execution using C++11 lambda.
+    image3.forEach<Pixel>
+    (
+      [](Pixel &pixel, const int * position) -> void
+      {
+        complicatedThreshold(pixel);
+      }
+     );
+  }
+
+#endif
