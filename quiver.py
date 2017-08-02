4dd7f01df123669329e4e040f647e6a8e3c575ef
ddkhoa87 <ddkhoa87@gmail.com>
2017-08-02 15:24:37 +0700

    Added ImgCoordDisplay

--- quiver.py
+++ quiver.py
@@ -18,20 +18,82 @@ def quiver():
 
 def radialDistortionPlot():
     
-    x1 = np.linspace(-1,1,21)
-    y1 = np.linspace(-1,1,21)
+    x1 = np.linspace(-1,1,31)
+    y1 = np.linspace(-1,1,31)
 
     x, y = np.meshgrid(x1, y1)
 
     r2 = np.add( np.multiply(x,x), np.multiply(y,y) )
     
-    k = np.multiply( r2, 0.05 )
+    # Multiplied by >0 -> barrel, <0 -> pincushion
+
+    fig = plt.figure(1, figsize=(13,5))
+    
+    # Pincushion
+    k = np.multiply( r2, -0.03 )
     
     u = np.multiply( x, k )
     v = np.multiply( y, k )
 
-    fig = plt.figure()
+    fig1 = plt.subplot("121")
+    fig1.set_title('k < 0')
     plt.quiver(x, y, u, v, scale=1)
+
+    # Barrel
+    k = np.multiply( r2, 0.03 )
+    
+    u = np.multiply( x, k )
+    v = np.multiply( y, k )
+
+    fig2 = plt.subplot(122)
+    fig2.set_title('k > 0')
+    plt.quiver(x, y, u, v, scale=1)
+
+    # Draw two figures
+    plt.draw()
+
+    plt.pause(1)
+    input("<Hit Enter To Close>")
+    plt.close(fig)
+
+def drawMeshes():
+    x1 = np.linspace(-1,1,31)
+    y1 = np.linspace(-1,1,31)
+
+    x, y = np.meshgrid(x1, y1)
+
+    r2 = np.add( np.multiply(x,x), np.multiply(y,y) )
+    
+    # Multiplied by >0 -> barrel, <0 -> pincushion
+
+    fig = plt.figure(2, figsize=(13,5))
+    
+    # Pincushion
+    fig1 = plt.subplot("121")
+    fig1.set_title('Original')
+    plt.scatter(x, y, s = 2)
+
+    # Barrel
+    k = np.multiply( r2, -0.2 )
+    
+    u = np.multiply( x, k )
+    v = np.multiply( y, k )
+
+    fig2 = plt.subplot(122)
+    fig2.set_title('k < 0')
+
+    # Make some adjust ment
+    #u = np.add(u, 0.5)
+    #v = np.add(v, 0.5)
+
+    #plt.scatter( u[10:20], v[10:20], s = 2)
+    plt.scatter( np.add(x, u), np.add(y, v), s = 2)
+
+    # save to txt
+    np.savetxt('x', x, fmt='%.1f')
+    np.savetxt('u', u, fmt='%.1f')
+
+    # Draw two figures
     plt.draw()
 
     plt.pause(1)
@@ -39,3 +101,4 @@ def radialDistortionPlot():
     plt.close(fig)
 
 radialDistortionPlot()
+drawMeshes()
